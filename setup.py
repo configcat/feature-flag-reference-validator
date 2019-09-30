661b70f32a21cb8ce1c897d90f0a9609a661848e
ConfigCat <developer@configcat.com>
2019-09-30 23:31:28 +0200

    Fix pypi deploy issues

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
 import setuptools
 
-
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
-
-version = "1.0.2"
-
+version = "1.0.3"
 
 setuptools.setup(
     name='configcat-flag-reference-validator',
@@ -18,8 +11,8 @@ setuptools.setup(
     license='MIT',
     author='ConfigCat',
     author_email='developer@configcat.com',
-    description='ConfigCat flag reference validator.',
-    long_description=readme(),
+    description='ConfigCat feature flag reference validator.',
+    long_description="This tool can be used for discovering ConfigCat feature flag usages in your source code and validating them against your own ConfigCat configuration dashboard. It searches for ConfigCat SDK usage and greps the feature flag keys from the source code, then it compares them with the keys got from your ConfigCat dashboard.",
     long_description_content_type="text/markdown",
     install_requires=['requests>=2.19.1'],
     classifiers=[
