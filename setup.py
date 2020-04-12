545db5c203087216806024330160d85e2d4dc7ab
sige <sige@configcat.com>
2020-04-12 15:12:17 +0200

    description

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-version = "2.0.0"
+version = "2.0.1"
 
 setuptools.setup(
     name='configcat-flag-reference-validator',
@@ -10,9 +10,9 @@ setuptools.setup(
     url='https://github.com/configcat/feature-flag-reference-validator',
     license='MIT',
     author='ConfigCat',
-    author_email='developer@configcat.com',
+    author_email='team@configcat.com',
     description='ConfigCat feature flag reference validator.',
-    long_description="This tool can be used for discovering ConfigCat feature flag usages in your source code and validating them against your own ConfigCat configuration dashboard. It searches for ConfigCat SDK usage and greps the feature flag keys from the source code, then it compares them with the keys got from your ConfigCat dashboard.",
+    long_description="This tool can be used for discovering ConfigCat feature flag usages in your source code and validating them against your own ConfigCat configuration dashboard. It searches for ConfigCat SDK usage and greps the feature flag keys from the source code, then it compares them with the keys got from your ConfigCat dashboard. Manage features and change your software configuration using ConfigCat feature flags, without the need to re-deploy code. A 10 minute trainable dashboard allows even non-technical team members to manage application features. Supports A/B testing, soft launching or targeting a specific group of users first with new ideas. Deploy any time, release when confident. Open-source SDKs enable easy integration with any web, mobile or backend application. https://configcat.com",
     install_requires=['requests>=2.19.1'],
     classifiers=[
         'Intended Audience :: Developers',
