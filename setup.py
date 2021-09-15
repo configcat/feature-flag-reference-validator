f324af47e3d866d04e1a16d285a80d708de2b51d
Endre Toth <endre.toth@logmein.com>
2021-09-16 00:06:08 +0200

    security update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     author_email='team@configcat.com',
     description='ConfigCat feature flag reference validator.',
     long_description="This tool can be used for discovering ConfigCat feature flag usages in your source code and validating them against your own ConfigCat configuration dashboard. It searches for ConfigCat SDK usage and greps the feature flag keys from the source code, then it compares them with the keys got from your ConfigCat dashboard. Manage features and change your software configuration using ConfigCat feature flags, without the need to re-deploy code. A 10 minute trainable dashboard allows even non-technical team members to manage application features. Supports A/B testing, soft launching or targeting a specific group of users first with new ideas. Deploy any time, release when confident. Open-source SDKs enable easy integration with any web, mobile or backend application. https://configcat.com",
-    install_requires=['requests>=2.19.1'],
+    install_requires=['requests>=2.25.1'],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
